9771ea7bf586552e9185fc4bbf9ee575d4a5f747
Rafael H. Schloming <rhs@apache.org>
2012-05-03 17:24:17 +0000

    cleaned up header file
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/trunk@1333560 13f79535-47bb-0310-9956-ffa450edef68

--- proton-c/include/proton/driver.h
+++ proton-c/include/proton/driver.h
@@ -31,12 +31,12 @@ typedef struct pn_listener_t pn_listener_t;
 typedef struct pn_connector_t pn_connector_t;
 
 pn_driver_t *pn_driver(void);
-void pn_driver_trace(pn_driver_t *d, pn_trace_t trace);
-void pn_driver_wakeup(pn_driver_t *d);
-void pn_driver_wait(pn_driver_t *d, int timeout);
-pn_listener_t *pn_driver_listener(pn_driver_t *d);
-pn_connector_t *pn_driver_connector(pn_driver_t *d);
-void pn_driver_destroy(pn_driver_t *d);
+void pn_driver_trace(pn_driver_t *driver, pn_trace_t trace);
+void pn_driver_wakeup(pn_driver_t *driver);
+void pn_driver_wait(pn_driver_t *driver, int timeout);
+pn_listener_t *pn_driver_listener(pn_driver_t *driver);
+pn_connector_t *pn_driver_connector(pn_driver_t *driver);
+void pn_driver_destroy(pn_driver_t *driver);
 
 pn_listener_t *pn_listener(pn_driver_t *driver, const char *host,
                            const char *port, void* context);
@@ -50,15 +50,15 @@ void pn_listener_destroy(pn_listener_t *listener);
 pn_connector_t *pn_connector(pn_driver_t *driver, const char *host,
                              const char *port, void* context);
 pn_connector_t *pn_connector_fd(pn_driver_t *driver, int fd, void *context);
-void pn_connector_trace(pn_connector_t *ctor, pn_trace_t trace);
-void pn_connector_process(pn_connector_t *ctor);
-pn_listener_t *pn_connector_listener(pn_connector_t *ctor);
-pn_sasl_t *pn_connector_sasl(pn_connector_t *ctor);
-pn_connection_t *pn_connector_connection(pn_connector_t *ctor);
-void *pn_connector_context(pn_connector_t *ctor);
-void pn_connector_set_context(pn_connector_t *ctor, void *context);
-void pn_connector_close(pn_connector_t *ctor);
-bool pn_connector_closed(pn_connector_t *ctor);
-void pn_connector_destroy(pn_connector_t *ctor);
+void pn_connector_trace(pn_connector_t *connector, pn_trace_t trace);
+void pn_connector_process(pn_connector_t *connector);
+pn_listener_t *pn_connector_listener(pn_connector_t *connector);
+pn_sasl_t *pn_connector_sasl(pn_connector_t *connector);
+pn_connection_t *pn_connector_connection(pn_connector_t *connector);
+void *pn_connector_context(pn_connector_t *connector);
+void pn_connector_set_context(pn_connector_t *connector, void *context);
+void pn_connector_close(pn_connector_t *connector);
+bool pn_connector_closed(pn_connector_t *connector);
+void pn_connector_destroy(pn_connector_t *connector);
 
 #endif /* driver.h */
