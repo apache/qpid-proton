000754ccf50f6c7c9376167e8d94c9b60b99706a
Rafael H. Schloming <rhs@apache.org>
2012-05-30 10:14:50 +0000

    integrated doxygen patch from kgiusti
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/trunk@1344181 13f79535-47bb-0310-9956-ffa450edef68

--- proton-c/include/proton/driver.h
+++ proton-c/include/proton/driver.h
@@ -26,40 +26,251 @@
 #include <proton/engine.h>
 #include <proton/sasl.h>
 
+/** @file
+ * API for the Driver Layer.
+ *
+ * The driver library provides a simple implementation of a driver for
+ * the proton engine. A driver is responsible for providing input,
+ * output, and tick events to the bottom half of the engine API. See
+ * ::pn_input, ::pn_output, and ::pn_tick. The driver also provides an
+ * interface for the application to access the top half of the API
+ * when the state of the engine may have changed due to I/O or timing
+ * events. Additionally the driver incorporates the SASL engine as
+ * well in order to provide a complete network stack: AMQP over SASL
+ * over TCP.
+ *
+ */
+
 typedef struct pn_driver_t pn_driver_t;
 typedef struct pn_listener_t pn_listener_t;
 typedef struct pn_connector_t pn_connector_t;
 
+/** Construct a driver
+ *
+ *  Call pn_driver_destroy() to release the driver object.
+ *  @return new driver object, NULL if error
+ */
 pn_driver_t *pn_driver(void);
+
+/** Set the tracing level for the given driver.
+ *
+ * @param[in] driver the driver to trace
+ * @param[in] trace the trace level to use.
+ * @todo pn_trace_t needs documentation
+ */
 void pn_driver_trace(pn_driver_t *driver, pn_trace_t trace);
+
+/** Force pn_driver_wait() to return
+ *
+ * @param[in] driver the driver to wake up
+ */
 void pn_driver_wakeup(pn_driver_t *driver);
+
+/** Wait for an active connector or listener
+ *
+ * @param[in] driver the driver to wait on
+ * @param[in] timeout maximum time in milliseconds to wait, -1 means
+ *                    infinite wait
+ */
 void pn_driver_wait(pn_driver_t *driver, int timeout);
+
+/** Get the next listener with pending data in the driver.
+ *
+ * @param[in] driver the driver
+ * @return NULL if no active listener available
+ */
 pn_listener_t *pn_driver_listener(pn_driver_t *driver);
+
+/** Get the next active connector in the driver.
+ *
+ * Returns the next connector with pending inbound data, available
+ * capacity for outbound data, or pending tick.
+ *
+ * @param[in] driver the driver
+ * @return NULL if no active connector available
+ */
 pn_connector_t *pn_driver_connector(pn_driver_t *driver);
+
+/** Destruct the driver allocated via pn_driver, and all associated
+ *  listeners and connectors.
+ *
+ * @param[in] driver the driver to destruct, no longer valid on
+ *                   return
+ */
 void pn_driver_destroy(pn_driver_t *driver);
 
+
+/** pn_listener - the server API **/
+
+/** Construct a listener for the given address.
+ *
+ * @param[in] driver driver that will 'own' this listener
+ * @param[in] host local host address to listen on
+ * @param[in] port local port to listen on
+ * @param[in] context application-supplied, can be accessed via
+ *                    pn_listener_context()
+ * @return a new listener on the given host:port, NULL if error
+ */
 pn_listener_t *pn_listener(pn_driver_t *driver, const char *host,
                            const char *port, void* context);
+
+/** Create a listener using the existing file descriptor.
+ *
+ * @param[in] driver driver that will 'own' this listener
+ * @param[in] fd existing file descriptor for listener to listen on
+ * @param[in] context application-supplied, can be accessed via
+ *                    pn_listener_context()
+ * @return a new listener on the given host:port, NULL if error
+ */
 pn_listener_t *pn_listener_fd(pn_driver_t *driver, int fd, void *context);
+
+/**
+ * @todo pn_listener_trace needs documentation
+ */
 void pn_listener_trace(pn_listener_t *listener, pn_trace_t trace);
+
+/** Accept a connection that is pending on the listener.
+ *
+ * @param[in] listener the listener to accept the connection on
+ * @return a new connector for the remote, or NULL on error
+ */
 pn_connector_t *pn_listener_accept(pn_listener_t *listener);
+
+/** Access the application context that is associated with the listener.
+ *
+ * @param[in] listener the listener whose context is to be returned
+ * @return the application context that was passed to pn_listener() or
+ *         pn_listener_fd()
+ */
 void *pn_listener_context(pn_listener_t *listener);
+
+/** Close the socket used by the listener.
+ *
+ * @param[in] listener the listener whose socket will be closed.
+ */
 void pn_listener_close(pn_listener_t *listener);
+
+/** Destructor for the given listener.
+ *
+ * Assumes the listener's socket has been closed prior to call.
+ *
+ * @param[in] listener the listener object to destroy, no longer valid
+ *            on return
+ */
 void pn_listener_destroy(pn_listener_t *listener);
 
+
+/** pn_connector - the client API **/
+
+/** Construct a connector to the given remote address.
+ *
+ * @param[in] driver owner of this connection.
+ * @param[in] host remote host to connect to.
+ * @param[in] port remote port to connect to.
+ * @param[in] context application supplied, can be accessed via
+ *                    pn_connector_context() @return a new connector
+ *                    to the given remote, or NULL on error.
+ */
 pn_connector_t *pn_connector(pn_driver_t *driver, const char *host,
                              const char *port, void* context);
+
+/** Create a connector using the existing file descriptor.
+ *
+ * @param[in] driver driver that will 'own' this connector.
+ * @param[in] fd existing file descriptor to use for this connector.
+ * @param[in] context application-supplied, can be accessed via
+ *                    pn_connector_context()
+ * @return a new connector to the given host:port, NULL if error.
+ */
 pn_connector_t *pn_connector_fd(pn_driver_t *driver, int fd, void *context);
+
+/** Set the tracing level for the given connector.
+ *
+ * @param[in] connector the connector to trace
+ * @param[in] trace the trace level to use.
+ */
 void pn_connector_trace(pn_connector_t *connector, pn_trace_t trace);
+
+/** Service the given connector.
+ *
+ * Handle any inbound data, outbound data, or timing events pending on
+ * the connector.
+ *
+ * @param[in] connector the connector to process.
+ */
 void pn_connector_process(pn_connector_t *connector);
+
+/** Access the listener which opened this connector.
+ *
+ * @param[in] connector connector whose listener will be returned.
+ * @return the listener which created this connector, or NULL if the
+ *         connector has no listener (e.g. an outbound client
+ *         connection)
+ */
 pn_listener_t *pn_connector_listener(pn_connector_t *connector);
+
+/** Access the Authentication and Security context of the connector.
+ *
+ * @param[in] connector connector whose securty context will be
+ *                      returned
+ * @return the Authentication and Security context for the connector,
+ *         or NULL if none
+ */
 pn_sasl_t *pn_connector_sasl(pn_connector_t *connector);
+
+/** Access the AMQP Connection associated with the connector.
+ *
+ * @param[in] connector the connector whose connection will be
+ *                      returned
+ * @return the connection context for the connector, or NULL if none
+ */
 pn_connection_t *pn_connector_connection(pn_connector_t *connector);
+
+/** Assign the AMQP Connection associated with the connector.
+ *
+ * @param[in] connector the connector whose connection will be set.
+ * @param[in] connection the connection to associate with the
+ *                       connector
+ */
 void pn_connector_set_connection(pn_connector_t *ctor, pn_connection_t *connection);
+
+/** Access the application context that is associated with the
+ *  connector.
+ *
+ * @param[in] connector the connector whose context is to be returned.
+
+ * @return the application context that was passed to pn_connector()
+ *         or pn_connector_fd()
+ */
 void *pn_connector_context(pn_connector_t *connector);
+
+/** Assign a new application context to the connector.
+ *
+ * @param[in] connector the connector which will hold the context.
+ * @param[in] context new application context to associate with the
+ *                    connector
+ */
 void pn_connector_set_context(pn_connector_t *connector, void *context);
+
+/** Close the socket used by the connector.
+ *
+ * @param[in] connector the connector whose socket will be closed
+ */
 void pn_connector_close(pn_connector_t *connector);
+
+/** Determine if the connector is closed.
+ *
+ * @return True if closed, otherwise false
+ */
 bool pn_connector_closed(pn_connector_t *connector);
+
+/** Destructor for the given connector.
+ *
+ * Assumes the connector's socket has been closed prior to call.
+ *
+ * @param[in] connector the connector object to destroy. No longer
+ *                      valid on return
+ */
 void pn_connector_destroy(pn_connector_t *connector);
 
 #endif /* driver.h */
--- proton-c/include/proton/engine.h
+++ proton-c/include/proton/engine.h
@@ -27,11 +27,17 @@
 #include <sys/types.h>
 #include <proton/errors.h>
 
+/** @file
+ * API for the proton Engine.
+ *
+ * @todo
+ */
+
 typedef struct pn_error_t pn_error_t;
 typedef struct pn_transport_t pn_transport_t;
-typedef struct pn_connection_t pn_connection_t;
-typedef struct pn_session_t pn_session_t;
-typedef struct pn_link_t pn_link_t;
+typedef struct pn_connection_t pn_connection_t; /**< Connection */
+typedef struct pn_session_t pn_session_t;       /**< Session */
+typedef struct pn_link_t pn_link_t;             /**< Link */
 typedef struct pn_delivery_t pn_delivery_t;
 
 typedef struct pn_delivery_tag_t {
@@ -41,18 +47,23 @@ typedef struct pn_delivery_tag_t {
 
 #define pn_dtag(BYTES, SIZE) ((pn_delivery_tag_t) {(SIZE), (BYTES)})
 
-typedef int pn_state_t;
+typedef int pn_state_t;     /**< encodes the state of an endpoint */
 
-#define PN_LOCAL_UNINIT (1)
-#define PN_LOCAL_ACTIVE (2)
-#define PN_LOCAL_CLOSED (4)
-#define PN_REMOTE_UNINIT (8)
-#define PN_REMOTE_ACTIVE (16)
-#define PN_REMOTE_CLOSED (32)
+#define PN_LOCAL_UNINIT (1)    /**< local endpoint requires initialization */
+#define PN_LOCAL_ACTIVE (2)    /**< local endpoint is active */
+#define PN_LOCAL_CLOSED (4)    /**< local endpoint is closed */
+#define PN_REMOTE_UNINIT (8)   /**< remote endpoint pending initialization by peer */
+#define PN_REMOTE_ACTIVE (16)  /**< remote endpoint is active */
+#define PN_REMOTE_CLOSED (32)  /**< remote endpoint has closed */
 
 #define PN_LOCAL_MASK (PN_LOCAL_UNINIT | PN_LOCAL_ACTIVE | PN_LOCAL_CLOSED)
 #define PN_REMOTE_MASK (PN_REMOTE_UNINIT | PN_REMOTE_ACTIVE | PN_REMOTE_CLOSED)
 
+/** @enum pn_disposition_t
+ * The state/outcome of a message transfer.
+ *
+ * @todo document each value
+ */
 typedef enum pn_disposition_t {
   PN_RECEIVED=1,
   PN_ACCEPTED=2,
@@ -68,25 +79,135 @@ typedef int pn_trace_t;
 #define PN_TRACE_FRM (2)
 
 // connection
+
+/** Factory to construct a new Connection.
+ *
+ * @return pointer to a new connection object.
+ */
 pn_connection_t *pn_connection();
 
+/** Retrieve the state of the connection.
+ *
+ * @param[in] connection the connection
+ * @return the connection's state flags
+ */
 pn_state_t pn_connection_state(pn_connection_t *connection);
+/** @todo: needs documentation */
 pn_error_t *pn_connection_error(pn_connection_t *connection);
+/** @todo: needs documentation */
 char *pn_connection_container(pn_connection_t *connection);
+/** @todo: needs documentation */
 void pn_connection_set_container(pn_connection_t *connection, const char *container);
+/** @todo: needs documentation */
 char *pn_connection_hostname(pn_connection_t *connection);
+/** @todo: needs documentation */
 void pn_connection_set_hostname(pn_connection_t *connection, const char *hostname);
 
+/** Extracts the first delivery on the connection that has pending
+ *  operations.
+ *
+ * Retrieves the first delivery on the Connection that has pending
+ * operations. A readable delivery indicates message data is waiting
+ * to be read. A writable delivery indicates that message data may be
+ * sent. An updated delivery indicates that the delivery's disposition
+ * has changed. A delivery will never be both readable and writible,
+ * but it may be both readable and updated or both writiable and
+ * updated.
+ *
+ * @param[in] connection the connection
+ * @return the first delivery object that needs to be serviced, else
+ * NULL if none
+ */
 pn_delivery_t *pn_work_head(pn_connection_t *connection);
+
+/** Get the next delivery on the connection that needs has pending
+ *  operations.
+ *
+ * @param[in] delivery the previous delivery retrieved from
+ *                     either pn_work_head() or pn_work_next()
+ * @return the next delivery that has pending operations, else
+ * NULL if none
+ */
 pn_delivery_t *pn_work_next(pn_delivery_t *delivery);
 
+/** Factory for creating a new session on the connection.
+ *
+ * A new session is created for the connection, and is added to the
+ * set of sessions maintained by the connection.
+ *
+ * @param[in] connection the session will exist over this connection
+ * @return pointer to new session
+ */
 pn_session_t *pn_session(pn_connection_t *connection);
+
+/** Factory for creating the connection's transport.
+ *
+ * The transport used by the connection to interface with the network.
+ * There can only be one transport associated with a connection.
+ *
+ * @param[in] connection connection that will use the transport
+ * @return pointer to new session
+ */
 pn_transport_t *pn_transport(pn_connection_t *connection);
 
+/** Retrieve the first Session that matches the given state mask.
+ *
+ * Examines the state of each session owned by the connection, and
+ * returns the first Session that matches the given state mask. If
+ * state contains both local and remote flags, then an exact match
+ * against those flags is performed. If state contains only local or
+ * only remote flags, then a match occurs if any of the local or
+ * remote flags are set respectively.
+ *
+ * @param[in] connection to be searched for matching sessions
+ * @param[in] state mask to match
+ * @return the first session owned by the connection that matches the
+ * mask, else NULL if no sessions match
+ */
 pn_session_t *pn_session_head(pn_connection_t *connection, pn_state_t state);
+
+/** Retrieve the next Session that matches the given state mask.
+ *
+ * When used with pn_session_head(), application can access all
+ * Sessions on the connection that match the given state. See
+ * pn_session_head() for description of match behavior.
+ *
+ * @param[in] session the previous session obtained from
+ *                    pn_session_head() or pn_session_next()
+ * @param[in] state mask to match.
+ * @return the next session owned by the connection that matches the
+ * mask, else NULL if no sessions match
+ */
 pn_session_t *pn_session_next(pn_session_t *session, pn_state_t state);
 
+/** Retrieve the first Link that matches the given state mask.
+ *
+ * Examines the state of each Link owned by the connection and returns
+ * the first Link that matches the given state mask. If state contains
+ * both local and remote flags, then an exact match against those
+ * flags is performed. If state contains only local or only remote
+ * flags, then a match occurs if any of the local or remote flags are
+ * set respectively.
+ *
+ * @param[in] connection to be searched for matching Links
+ * @param[in] state mask to match
+ * @return the first Link owned by the connection that matches the
+ * mask, else NULL if no Links match
+ */
 pn_link_t *pn_link_head(pn_connection_t *connection, pn_state_t state);
+
+/** Retrieve the next Link that matches the given state mask.
+ *
+ * When used with pn_link_head(), the application can access all Links
+ * on the connection that match the given state. See pn_link_head()
+ * for description of match behavior.
+ *
+ * @param[in] link the previous Link obtained from pn_link_head() or
+ *                 pn_link_next()
+ * @param[in] state mask to match
+ * @return the next session owned by the connection that matches the
+ * mask, else NULL if no sessions match
+ */
 pn_link_t *pn_link_next(pn_link_t *link, pn_state_t state);
 
 void pn_connection_open(pn_connection_t *connection);
