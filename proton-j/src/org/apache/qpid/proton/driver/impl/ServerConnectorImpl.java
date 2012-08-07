f37f15f2728aa47f7b9cca14d98b3916bb75be87
Rajith Muditha Attapattu <rajith@apache.org>
2012-08-07 11:01:58 +0000

    NO-JIRA Added license headers.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/branches/rajith_sandbox@1370171 13f79535-47bb-0310-9956-ffa450edef68

--- proton-j/src/org/apache/qpid/proton/driver/impl/ServerConnectorImpl.java
+++ proton-j/src/org/apache/qpid/proton/driver/impl/ServerConnectorImpl.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.qpid.proton.driver.impl;
 
 import static org.apache.qpid.proton.driver.impl.ServerConnectorImpl.ConnectorState.UNINITIALIZED;
@@ -11,7 +27,6 @@ import org.apache.qpid.proton.driver.Connector;
 import org.apache.qpid.proton.driver.Listener;
 import org.apache.qpid.proton.engine.Connection;
 import org.apache.qpid.proton.engine.Sasl;
-import org.apache.qpid.proton.engine.impl.SaslServerImpl;
 import org.apache.qpid.proton.logging.LogHandler;
 
 class ServerConnectorImpl<C> implements Connector<C>
