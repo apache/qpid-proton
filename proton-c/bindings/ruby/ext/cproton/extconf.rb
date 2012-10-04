ccc89e28069a9858b7cd9e374bb45c0c1c012149
mcpierce <mcpierce@unknown>
2012-10-04 19:54:41 +0000

    PROTON-3 : Provides a means for packaging Proton as a Ruby gem.
    The gem's name is qpid_proton (since proton was already taken).
    
    Provides a Rakefile with two targets:
     1. generates the cproton.c file from the swig bindings file, and
     2. packages up everything into a gem file for distribution.
    
    Also provides the basic Proton ruby library files.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/trunk@1394226 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proton-c/bindings/ruby/ext/cproton/extconf.rb
@@ -0,0 +1,45 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+require 'mkmf'
+
+dir_config("qpid-proton")
+
+REQUIRED_LIBRARIES = [
+                      "qpid-proton",
+                     ]
+
+REQUIRED_LIBRARIES.each do |library|
+  abort "Missing library: #{library}" unless have_library library
+end
+
+REQUIRED_HEADERS = [
+                    "proton/engine.h",
+                    "proton/message.h",
+                    "proton/sasl.h",
+                    "proton/driver.h",
+                    "proton/messenger.h"
+                   ]
+
+REQUIRED_HEADERS.each do |header|
+  abort "Missing header: #{header}" unless have_header header
+end
+
+create_makefile('cproton')
+
