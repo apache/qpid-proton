3dea54156d7459cf88ab197e40f4c9f99a683616
Keith Wall <kwall@apache.org>
2013-03-07 13:17:30 +0000

    PROTON-257: Added test that illustrates use of the Engine and Message proton-j APIs. Also added LoggingProtocolTracer and TestLoggingHelper. Configured Java Logging during test runs initiated by Maven.
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/trunk@1453852 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/java/org/apache/qpid/proton/systemtests/ProtonContainer.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.qpid.proton.systemtests;
+
+import org.apache.qpid.proton.amqp.messaging.Source;
+import org.apache.qpid.proton.amqp.messaging.Target;
+import org.apache.qpid.proton.engine.Connection;
+import org.apache.qpid.proton.engine.Delivery;
+import org.apache.qpid.proton.engine.Receiver;
+import org.apache.qpid.proton.engine.Sender;
+import org.apache.qpid.proton.engine.Session;
+import org.apache.qpid.proton.engine.Transport;
+import org.apache.qpid.proton.message.Message;
+
+/**
+ * Simple "struct" class used to hold related Engine objects
+ */
+public class ProtonContainer
+{
+    String containerId;
+    Connection connection;
+    Transport transport;
+    Session session;
+    Sender sender;
+    Receiver receiver;
+    Source source;
+    Target target;
+    Delivery delivery;
+    Message message;
+    byte[] messageData;
+
+    public ProtonContainer(String containerId)
+    {
+        this.containerId = containerId;
+    }
+}
