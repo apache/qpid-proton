8fc9f41de01057edb35a7cfb8d9f6e062a0b8128
mcpierce <mcpierce@unknown>
2013-09-24 18:22:26 +0000

    PROTON-427: Removed the flag argument from Messenger.settle
    Made the API more consistent with the accept and reject methods.
    
    git-svn-id: https://svn.apache.org/repos/asf/qpid/proton/trunk@1525959 13f79535-47bb-0310-9956-ffa450edef68

--- proton-c/bindings/ruby/lib/qpid_proton/messenger.rb
+++ proton-c/bindings/ruby/lib/qpid_proton/messenger.rb
@@ -338,13 +338,17 @@ module Qpid
       # ==== Options
       #
       # * tracker - the tracker
-      # * flag - the flag
       #
       # ==== Examples
       #
-      def settle(tracker, flag)
+      def settle(tracker)
         raise TypeError.new("invalid tracker: #{tracker}") unless valid_tracker?(tracker)
-        raise TypeError.new("invalid flag: #{flag}") unless Qpid::Proton::Tracker.valid_flag?(flag)
+        if tracker.nil? then
+          tracker = self.incoming_tracker
+          flag = Cproton::PN_CUMULATIVE
+        else
+          flag = 0
+        end
         Cproton.pn_messenger_settle(@impl, tracker.impl, flag)
       end
 
