cdee05455979b9f6fd5f31d4842d75e2ff5f3a94
Gordon Sim <gsim@redhat.com>
2015-02-06 09:46:01 +0000

    Reverse previous indentation changes to legacy uuid support and instead alter the definition of generate_uuid accordingly

--- proton-c/bindings/python/proton/__init__.py
+++ proton-c/bindings/python/proton/__init__.py
@@ -36,6 +36,10 @@ from wrapper import Wrapper
 import weakref, socket, sys
 try:
   import uuid
+
+  def generate_uuid():
+    return uuid.uuid4()
+
 except ImportError:
   """
   No 'native' UUID support.  Provide a very basic UUID type that is a compatible subset of the uuid type provided by more modern python releases.
@@ -68,26 +72,26 @@ except ImportError:
       def __hash__(self):
         return self.bytes.__hash__()
 
-    import os, random, time
-    rand = random.Random()
-    rand.seed((os.getpid(), time.time(), socket.gethostname()))
-    def random_uuid():
-      bytes = [rand.randint(0, 255) for i in xrange(16)]
+  import os, random, time
+  rand = random.Random()
+  rand.seed((os.getpid(), time.time(), socket.gethostname()))
+  def random_uuid():
+    bytes = [rand.randint(0, 255) for i in xrange(16)]
 
-      # From RFC4122, the version bits are set to 0100
-      bytes[7] &= 0x0F
-      bytes[7] |= 0x40
+    # From RFC4122, the version bits are set to 0100
+    bytes[7] &= 0x0F
+    bytes[7] |= 0x40
 
-      # From RFC4122, the top two bits of byte 8 get set to 01
-      bytes[8] &= 0x3F
-      bytes[8] |= 0x80
-      return "".join(map(chr, bytes))
+    # From RFC4122, the top two bits of byte 8 get set to 01
+    bytes[8] &= 0x3F
+    bytes[8] |= 0x80
+    return "".join(map(chr, bytes))
 
-    def uuid4():
-      return uuid.UUID(bytes=random_uuid())
+  def uuid4():
+    return uuid.UUID(bytes=random_uuid())
 
-def generate_uuid():
-  return uuid.uuid4()
+  def generate_uuid():
+    return uuid4()
 
 try:
   bytes()
