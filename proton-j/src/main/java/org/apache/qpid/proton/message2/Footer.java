2c21b632a962750ec75a4c152d92cbc36e96d463
Rajith Attapattu <rajith@apache.org>
2015-02-20 20:21:10 -0500

    Adding the initial impl for Message types. Some types are missing, which will be added in due course.

---
+++ proton-j/src/main/java/org/apache/qpid/proton/message2/Footer.java
@@ -0,0 +1,73 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.apache.qpid.proton.message2;
+
+import java.util.Map;
+
+import org.apache.qpid.proton.codec2.CodecHelper;
+import org.apache.qpid.proton.codec2.DecodeException;
+import org.apache.qpid.proton.codec2.DescribedTypeFactory;
+import org.apache.qpid.proton.codec2.Encodable;
+import org.apache.qpid.proton.codec2.Encoder;
+
+public final class Footer implements Encodable
+{
+    public final static long DESCRIPTOR_LONG = 0x0000000000000078L;
+
+    public final static String DESCRIPTOR_STRING = "amqp:footer:map";
+
+    private final Map<Object, Object> _value;
+
+    public Footer(Map<Object, Object> value)
+    {
+        _value = value;
+    }
+
+    public Map<Object, Object> getValue()
+    {
+        return _value;
+    }
+
+    @Override
+    public void encode(Encoder encoder)
+    {
+        encoder.putDescriptor();
+        encoder.putUlong(DESCRIPTOR_LONG);
+        CodecHelper.encodeMap(encoder, _value);
+        encoder.end();
+    }
+
+    public static final class Factory implements DescribedTypeFactory
+    {
+        @SuppressWarnings("unchecked")
+        public Object create(Object in) throws DecodeException
+        {
+            return new Footer((Map<Object, Object>) in);
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Footer{" + _value + '}';
+    }
+}
