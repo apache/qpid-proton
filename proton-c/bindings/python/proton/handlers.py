apache/qpid-proton

commit 0c11d11cdb5f1185219b907d1ad9ba22451512cc

PROTON-827: go binding: partial implementation of concurrent messaging API with examples.
Please see proton-c/bindings/go/README.md for details of this update.

Implemented a good chunk of the concurrent Go messaging API with send.go and receive.go examples.
The examples work with event/broker.go (which uses the event API) and with the python
broker.py, simple_send.py and simple_recv.py examples.

proton-c/bindings/python/proton/handlers.py
@@ -543,7 +543,7 @@ class CFlowController(WrappedHandler):
     def __init__(self, window=1024):
         WrappedHandler.__init__(self, lambda: pn_flowcontroller(window))
 
-class CHandshaker(WrappedHandler):
+oclass CHandshaker(WrappedHandler):
 
     def __init__(self):
         WrappedHandler.__init__(self, pn_handshaker)

.gitignore
@@ -28,3 +28,6 @@ eclipse-classes
 
 # The usual location for proton-c build files
 proton-c/build
+
+# Executables built by go binding tests
+proton-c/bindings/go/bin

examples/go/event/broker.go
@@ -36,15 +36,29 @@ import (
 	"log"
 	"net"
 	"os"
+	"path"
 	"qpid.apache.org/proton"
 	"qpid.apache.org/proton/event"
+	"sync"
 )
 
-// panicIf is simplistic error handling for example code, not recommended practice.
-func panicIf(err error) {
-	if err != nil {
-		panic(err)
+// Command-line flags
+var addr = flag.String("addr", ":amqp", "Listening address")
+var verbose = flag.Int("verbose", 1, "Output level, 0 means none, higher means more")
+var full = flag.Bool("full", false, "Print full message not just body.")
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage: %s
+A simple broker-like demo. Queues are created automatically for sender or receiver addrsses.
+`, os.Args[0])
+		flag.PrintDefaults()
 	}
+	flag.Parse()
+	b := newBroker()
+	err := b.listen(*addr)
+	fatalIf(err)
 }
 
 // queue is a structure representing a queue.
@@ -54,18 +68,20 @@ type queue struct {
 	consumers map[event.Link]bool // Set of consumer links
 }
 
-func newQueue(name string) *queue {
-	debug.Printf("Create queue %s\n", name)
-	return &queue{name, list.New(), make(map[event.Link]bool)}
+type logLink event.Link // Wrapper to print links in format for logging
+
+func (ll logLink) String() string {
+	l := event.Link(ll)
+	return fmt.Sprintf("%s[%p]", l.Name(), l.Session().Connection().Pump())
 }
 
 func (q *queue) subscribe(link event.Link) {
-	debug.Printf("Subscribe to %s\n", q.name)
+	debug.Printf("link %s subscribed to queue %s", logLink(link), q.name)
 	q.consumers[link] = true
 }
 
 func (q *queue) unsubscribe(link event.Link) {
-	debug.Printf("Unsubscribe from %s\n", q.name)
+	debug.Printf("link %s unsubscribed from queue %s", logLink(link), q.name)
 	delete(q.consumers, link)
 }
 
@@ -73,30 +89,37 @@ func (q *queue) empty() bool {
 	return len(q.consumers) == 0 && q.messages.Len() == 0
 }
 
-func (q *queue) publish(message proton.Message) {
-	debug.Printf("Push to %s: %#v\n", q.name, message)
+func (q *queue) push(context *event.Pump, message proton.Message) {
 	q.messages.PushBack(message)
-	q.dispatch()
+	q.pop(context)
 }
 
-func (q *queue) dispatchTo(link event.Link) bool {
+func (q *queue) popTo(context *event.Pump, link event.Link) bool {
 	if q.messages.Len() != 0 && link.Credit() > 0 {
-		message := q.messages.Front().Value.(proton.Message)
-		debug.Printf("Pop from %s: %#v\n", q.name, message)
+		message := q.messages.Remove(q.messages.Front()).(proton.Message)
+		debug.Printf("link %s <- queue %s: %s", logLink(link), q.name, formatMessage{message})
 		// The first return parameter is an event.Delivery.
 		// The Deliver can be used to track message status, e.g. so we can re-delver on failure.
 		// This demo broker doesn't do that.
-		_, err := message.Send(link)
-		panicIf(err)
-		q.messages.Remove(q.messages.Front())
+		linkPump := link.Session().Connection().Pump()
+		if context == linkPump {
+			if context == nil {
+				log.Fatal("pop in nil context")
+			}
+			link.Send(message) // link is in the current pump, safe to call Send() direct
+		} else {
+			linkPump.Inject <- func() { // Inject to link's pump
+				link.Send(message) // FIXME aconway 2015-05-04: error handlig
+			}
+		}
 		return true
 	}
 	return false
 }
 
-func (q *queue) dispatch() (dispatched bool) {
+func (q *queue) pop(context *event.Pump) (popped bool) {
 	for c, _ := range q.consumers {
-		dispatched = dispatched || q.dispatchTo(c)
+		popped = popped || q.popTo(context, c)
 	}
 	return
 }
@@ -104,54 +127,79 @@ func (q *queue) dispatch() (dispatched bool) {
 // broker implements event.MessagingHandler and reacts to events by moving messages on or off queues.
 type broker struct {
 	queues map[string]*queue
-	pumps  map[*event.Pump]struct{} // Set of running event pumps (i.e. connections)
+	lock   sync.Mutex // FIXME aconway 2015-05-04: un-golike, better broker coming...
 }
 
 func newBroker() *broker {
-	return &broker{queues: make(map[string]*queue), pumps: make(map[*event.Pump]struct{})}
+	return &broker{queues: make(map[string]*queue)}
 }
 
 func (b *broker) getQueue(name string) *queue {
 	q := b.queues[name]
 	if q == nil {
-		q = newQueue(name)
+		debug.Printf("Create queue %s", name)
+		q = &queue{name, list.New(), make(map[event.Link]bool)}
 		b.queues[name] = q
 	}
 	return q
 }
 
+func (b *broker) unsubscribe(l event.Link) {
+	if l.IsSender() {
+		q := b.queues[l.RemoteSource().Address()]
+		if q != nil {
+			q.unsubscribe(l)
+			if q.empty() {
+				debug.Printf("Delete queue %s", q.name)
+				delete(b.queues, q.name)
+			}
+		}
+	}
+}
+
 func (b *broker) Handle(t event.MessagingEventType, e event.Event) error {
+	// FIXME aconway 2015-05-04: locking is un-golike, better example coming soon.
+	// Needed because Handle is called for multiple connections concurrently
+	// and the queue data structures are not thread safe.
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
 	switch t {
 
 	case event.MLinkOpening:
 		if e.Link().IsSender() {
-			// FIXME aconway 2015-03-23: handle dynamic consumers
-			b.getQueue(e.Link().RemoteSource().Address()).subscribe(e.Link())
+			q := b.getQueue(e.Link().RemoteSource().Address())
+			q.subscribe(e.Link())
 		}
 
 	case event.MLinkClosing:
-		if e.Link().IsSender() {
-			q := b.getQueue(e.Link().RemoteSource().Address())
-			q.unsubscribe(e.Link())
-			if q.empty() {
-				delete(b.queues, q.name)
-			}
+		b.unsubscribe(e.Link())
+
+	case event.MDisconnected:
+		fallthrough
+	case event.MConnectionClosing:
+		c := e.Connection()
+		for l := c.LinkHead(event.SRemoteActive); !l.IsNil(); l = l.Next(event.SRemoteActive) {
+			b.unsubscribe(l)
 		}
 
 	case event.MSendable:
-		b.getQueue(e.Link().RemoteSource().Address()).dispatchTo(e.Link())
+		q := b.getQueue(e.Link().RemoteSource().Address())
+		q.popTo(e.Connection().Pump(), e.Link())
 
 	case event.MMessage:
-		m, err := proton.EventMessage(e)
-		panicIf(err)
-		b.getQueue(e.Link().RemoteTarget().Address()).publish(m)
+		m, err := event.DecodeMessage(e)
+		fatalIf(err)
+		qname := e.Link().RemoteTarget().Address()
+		debug.Printf("link %s -> queue %s: %s", logLink(e.Link()), qname, formatMessage{m})
+		b.getQueue(qname).push(e.Connection().Pump(), m)
 	}
 	return nil
 }
 
 func (b *broker) listen(addr string) (err error) {
 	// Use the standard Go "net" package to listen for connections.
-	info.Printf("Listening on %s\n", addr)
+	info.Printf("Listening on %s", addr)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return err
@@ -160,46 +208,56 @@ func (b *broker) listen(addr string) (err error) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			info.Printf("Accept error: %s\n", err)
+			info.Printf("Accept error: %s", err)
 			continue
 		}
-		info.Printf("Accepted connection %s<-%s\n", conn.LocalAddr(), conn.RemoteAddr())
 		pump, err := event.NewPump(conn, event.NewMessagingDelegator(b))
-		panicIf(err)
+		fatalIf(err)
+		info.Printf("Accepted %s[%p]", pump, pump)
 		pump.Server()
-		b.pumps[pump] = struct{}{}
-		go pump.Run()
+		go func() {
+			pump.Run()
+			if pump.Error == nil {
+				info.Printf("Closed %s", pump)
+			} else {
+				info.Printf("Closed %s: %s", pump, pump.Error)
+			}
+		}()
 	}
 }
 
-var addr = flag.String("addr", ":amqp", "Listening address")
-var quiet = flag.Bool("quiet", false, "Don't print informational messages")
-var debugFlag = flag.Bool("debug", false, "Print debugging messages")
+// Logging
+func logger(prefix string, level int, w io.Writer) *log.Logger {
+	if *verbose >= level {
+		return log.New(w, prefix, 0)
+	}
+	return log.New(ioutil.Discard, "", 0)
+}
+
 var info, debug *log.Logger
 
-func output(enable bool) io.Writer {
-	if enable {
-		return os.Stdout
-	} else {
-		return ioutil.Discard
+func init() {
+	flag.Parse()
+	name := path.Base(os.Args[0])
+	log.SetFlags(0)
+	log.SetPrefix(fmt.Sprintf("%s: ", name))                      // Log errors on stderr.
+	info = logger(fmt.Sprintf("%s: ", name), 1, os.Stdout)        // Log info on stdout.
+	debug = logger(fmt.Sprintf("%s debug: ", name), 2, os.Stderr) // Log debug on stderr.
+}
+
+// Simple error handling for demo.
+func fatalIf(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `
-Usage: %s [queue ...]
-A simple broker. Queues are created automatically for sender or receiver addrsses.
-`, os.Args[0])
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-	debug = log.New(output(*debugFlag), "debug: ", log.Ltime)
-	info = log.New(output(!*quiet), "info: ", log.Ltime)
-	b := newBroker()
-	err := b.listen(*addr)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+type formatMessage struct{ m proton.Message }
+
+func (fm formatMessage) String() string {
+	if *full {
+		return fmt.Sprintf("%#v", fm.m)
+	} else {
+		return fmt.Sprintf("%#v", fm.m.Body())
 	}
 }

examples/go/receive.go
@@ -22,25 +22,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"math"
 	"net"
 	"os"
+	"path"
 	"qpid.apache.org/proton"
+	"qpid.apache.org/proton/messaging"
 	"sync"
 	"time"
 )
 
-// Simplistic error handling for demo. Not recommended.
-func panicIf(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 // Command-line flags
+var verbose = flag.Int("verbose", 1, "Output level, 0 means none, higher means more")
 var count = flag.Int64("count", 0, "Stop after receiving this many messages. 0 means unlimited.")
 var timeout = flag.Int64("time", 0, "Stop after this many seconds. 0 means unlimited.")
-var short = flag.Bool("short", false, "Short format of message: body only")
+var full = flag.Bool("full", false, "Print full message not just body.")
 
 func main() {
 	// Parse flags and arguments, print usage message on error.
@@ -76,29 +75,44 @@ Receive messages from all the listed URLs concurrently and print them.
 
 	wait.Add(len(urls)) // Wait for one goroutine per URL.
 
-	for _, urlStr := range urls {
-		// Start a goroutine to receive from urlStr
+	// Arrange to close all connections on exit
+	connections := make([]*messaging.Connection, len(urls))
+	defer func() {
+		for _, c := range connections {
+			if c != nil {
+				c.Close()
+			}
+		}
+	}()
+
+	for i, urlStr := range urls {
+		debug.Printf("Connecting to %s", urlStr)
 		go func(urlStr string) {
 			defer wait.Done()                   // Notify main() that this goroutine is done.
 			url, err := proton.ParseURL(urlStr) // Like net/url.Parse() but with AMQP defaults.
-			panicIf(err)
+			fatalIf(err)
 
 			// Open a standard Go net.Conn for the AMQP connection
 			conn, err := net.Dial("tcp", url.Host) // Note net.URL.Host is actually "host:port"
-			panicIf(err)
-			defer conn.Close() // Close conn on goroutine exit.
+			fatalIf(err)
 
-			pc, err := proton.Connect(conn) // This is our AMQP connection.
-			panicIf(err)
-			// We could 'defer pc.Close()' but conn.close() will automatically close the proton connection.
+			pc, err := messaging.Connect(conn) // This is our AMQP connection.
+			fatalIf(err)
+			connections[i] = pc
 
 			// For convenience a proton.Connection provides a DefaultSession()
 			// pc.Receiver() is equivalent to pc.DefaultSession().Receiver()
 			r, err := pc.Receiver(url.Path)
-			panicIf(err)
+			fatalIf(err)
 
-			for m := range r.Receive { // r.Receive is a channel to receive messages.
-				select {
+			for {
+				var m proton.Message
+				select { // Receive a message or stop.
+				case m = <-r.Receive:
+				case <-stop: // The program is stopping.
+					return
+				}
+				select { // Send m to main() or stop
 				case messages <- m: // Send m to main()
 				case <-stop: // The program is stopping.
 					return
@@ -106,24 +120,57 @@ Receive messages from all the listed URLs concurrently and print them.
 			}
 		}(urlStr)
 	}
+	info.Printf("Listening")
 
 	// time.After() returns a channel that will close when the timeout is up.
 	timer := time.After(duration)
 
 	// main() prints each message and checks for count or timeout being exceeded.
-	for i := *count; i > 0; i-- {
+	for i := int64(0); i < *count; i++ {
 		select {
 		case m := <-messages:
-			if *short {
-				fmt.Println(m.Body())
-			} else {
-				fmt.Printf("%#v\n\n", m)
-			}
+			debug.Print(formatMessage{m})
 		case <-timer: // Timeout has expired
 			i = 0
 		}
 	}
-
+	info.Printf("Received %d messages", *count)
 	close(stop) // Signal all goroutines to stop.
 	wait.Wait() // Wait for all goroutines to finish.
+}
+
+// Logging
+func logger(prefix string, level int, w io.Writer) *log.Logger {
+	if *verbose >= level {
+		return log.New(w, prefix, 0)
+	}
+	return log.New(ioutil.Discard, "", 0)
+}
+
+var info, debug *log.Logger
+
+func init() {
+	flag.Parse()
+	name := path.Base(os.Args[0])
+	log.SetFlags(0)                                               // Use default logger for errors.
+	log.SetPrefix(fmt.Sprintf("%s: ", name))                      // Log errors on stderr.
+	info = logger(fmt.Sprintf("%s: ", name), 1, os.Stdout)        // Log info on stdout.
+	debug = logger(fmt.Sprintf("%s debug: ", name), 2, os.Stderr) // Log debug on stderr.
+}
+
+// Simple error handling for demo.
+func fatalIf(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type formatMessage struct{ m proton.Message }
+
+func (fm formatMessage) String() string {
+	if *full {
+		return fmt.Sprintf("%#v", fm.m)
+	} else {
+		return fmt.Sprintf("%#v", fm.m.Body())
+	}
 }

examples/go/send.go
@@ -22,22 +22,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"math"
 	"net"
 	"os"
+	"path"
 	"qpid.apache.org/proton"
+	"qpid.apache.org/proton/messaging"
 	"sync"
 )
 
-// Simplistic error handling for demo. Not recommended.
-func panicIf(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 // Command-line flags
+var verbose = flag.Int("verbose", 1, "Output level, 0 means none, higher means more")
 var count = flag.Int64("count", 0, "Send this may messages per address. 0 means unlimited.")
+
+// Ack associates an info string with an acknowledgement
+type Ack struct {
+	ack  messaging.Acknowledgement
+	info string
+}
 
 func main() {
 	// Parse flags and arguments, print usage message on error.
@@ -53,45 +58,101 @@ To each URL, send the string "path-n" where n is the message number.
 	urls := flag.Args() // Non-flag arguments are URLs to receive from
 	if len(urls) == 0 {
 		flag.Usage()
-		fmt.Fprintf(os.Stderr, "No URL provided")
+		fmt.Fprintf(os.Stderr, "No URL provided\n")
 		os.Exit(1)
 	}
 	if *count == 0 {
 		*count = math.MaxInt64
 	}
 
+	// Create a channel to receive all the acknowledgements
+	acks := make(chan Ack)
+
 	// Create a goroutine for each URL that sends messages.
 	var wait sync.WaitGroup // Used by main() to wait for all goroutines to end.
 	wait.Add(len(urls))     // Wait for one goroutine per URL.
 
-	for _, urlStr := range urls {
-		// Start a goroutine to receive from urlStr
+	// Arrange to close all connections on exit
+	connections := make([]*messaging.Connection, len(urls))
+	defer func() {
+		for _, c := range connections {
+			c.Close()
+		}
+	}()
+
+	for i, urlStr := range urls {
+		url, err := proton.ParseURL(urlStr) // Like net/url.Parse() but with AMQP defaults.
+		fatalIf(err)
+		debug.Printf("Connecting to %v", url)
+
+		// Open a standard Go net.Conn for the AMQP connection
+		conn, err := net.Dial("tcp", url.Host) // Note net.URL.Host is actually "host:port"
+		fatalIf(err)
+
+		pc, err := messaging.Connect(conn) // This is our AMQP connection using conn.
+		fatalIf(err)
+		connections[i] = pc
+
+		// Start a goroutine to send to urlStr
 		go func(urlStr string) {
-			defer wait.Done()                   // Notify main() that this goroutine is done.
-			url, err := proton.ParseURL(urlStr) // Like net/url.Parse() but with AMQP defaults.
-			panicIf(err)
+			defer wait.Done() // Notify main() that this goroutine is done.
 
-			// Open a standard Go net.Conn for the AMQP connection
-			conn, err := net.Dial("tcp", url.Host) // Note net.URL.Host is actually "host:port"
-			panicIf(err)
-			defer conn.Close() // Close conn on goroutine exit.
-
-			pc, err := proton.Connect(conn) // This is our AMQP connection.
-			panicIf(err)
-			// We could 'defer pc.Close()' but conn.close() will automatically close the proton connection.
-
-			// For convenience a proton.Connection provides a DefaultSession()
-			// pc.Sender() is equivalent to pc.DefaultSession().Sender()
+			// FIXME aconway 2015-04-29: sessions, default sessions, senders...
+			// Create a sender using the path of the URL as the AMQP target address
 			s, err := pc.Sender(url.Path)
-			panicIf(err)
+			fatalIf(err)
 
 			for i := int64(0); i < *count; i++ {
 				m := proton.NewMessage()
-				m.SetBody(fmt.Sprintf("%v-%v", url.Path, i))
-				err := s.Send(m)
-				panicIf(err)
+				body := fmt.Sprintf("%v-%v", url.Path, i)
+				m.SetBody(body)
+				ack, err := s.Send(m)
+				fatalIf(err)
+				acks <- Ack{ack, body}
 			}
 		}(urlStr)
 	}
+
+	// Wait for all the acknowledgements
+	expect := int(*count) * len(urls)
+	debug.Printf("Started senders, expect %v acknowledgements", expect)
+	for i := 0; i < expect; i++ {
+		ack, ok := <-acks
+		if !ok {
+			info.Fatalf("acks channel closed after only %d acks\n", i)
+		}
+		d := <-ack.ack
+		debug.Printf("acknowledgement[%v] %v", i, ack.info)
+		if d != messaging.Accepted {
+			info.Printf("Unexpected disposition %v", d)
+		}
+	}
+	info.Printf("Received all %v acknowledgements", expect)
 	wait.Wait() // Wait for all goroutines to finish.
+}
+
+// Logging
+func logger(prefix string, level int, w io.Writer) *log.Logger {
+	if *verbose >= level {
+		return log.New(w, prefix, 0)
+	}
+	return log.New(ioutil.Discard, "", 0)
+}
+
+var info, debug *log.Logger
+
+func init() {
+	flag.Parse()
+	name := path.Base(os.Args[0])
+	log.SetFlags(0)                                               // Use default logger for errors.
+	log.SetPrefix(fmt.Sprintf("%s: ", name))                      // Log errors on stderr.
+	info = logger(fmt.Sprintf("%s: ", name), 1, os.Stdout)        // Log info on stdout.
+	debug = logger(fmt.Sprintf("%s debug: ", name), 2, os.Stderr) // Log debug on stderr.
+}
+
+// Simple error handling for demo.
+func fatalIf(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }

proton-c/bindings/go/src/Makefile
@@ -6,20 +6,11 @@
 GENERATED=qpid.apache.org/proton/event/wrappers_gen.go
 
 test: $(GENERATED)
-	go test $(GOFLAGS) qpid.apache.org/proton
-	go test $(GOFLAGS) qpid.apache.org/proton/event
+	go test -v $(TESTFLAGS) $(GOFLAGS) qpid.apache.org/proton
+	go test -v $(TESTFLAGS) $(GOFLAGS) qpid.apache.org/proton/event
+	go test -v $(TESTFLAGS) $(GOFLAGS) qpid.apache.org/proton/messaging
 
-rebuild: $(GENERATED)
-	go build -a $(GOFLAGS) qpid.apache.org/proton
-	go build -a $(GOFLAGS) qpid.apache.org/proton/event
-	go test $(GOFLAGS) -c -a qpid.apache.org/proton
-	go test $(GOFLAGS) -c -a qpid.apache.org/proton/event
-
-qpid.apache.org/proton/event/wrappers_gen.go: genwrap.go ../../../include/proton/*.h
+$(GENERATED): genwrap.go ../../../include/proton/*.h
 	go run genwrap.go
-
-broker: test
-	go build $(GOFLAGS) ~/proton/examples/go/event/broker.go
-	./broker
 
 

proton-c/bindings/go/src/genwrap.go
@@ -44,7 +44,11 @@ func mixedCase(s string) string {
 	return result
 }
 
-var templateFuncs = template.FuncMap{"mixedCase": mixedCase}
+func mixedCaseTrim(s, prefix string) string {
+	return mixedCase(strings.TrimPrefix(s, prefix))
+}
+
+var templateFuncs = template.FuncMap{"mixedCase": mixedCase, "mixedCaseTrim": mixedCaseTrim}
 
 func doTemplate(out io.Writer, data interface{}, tmpl string) {
 	panicIf(template.Must(template.New("").Funcs(templateFuncs).Parse(tmpl)).Execute(out, data))
@@ -73,7 +77,7 @@ const ({{range $values}}
 func (e {{mixedCase $enumName}}) String() string {
 	switch e {
 {{range $values}}
-	case C.{{.}}: return "{{mixedCase .}}" {{end}}
+	case C.{{.}}: return "{{mixedCaseTrim . "PN_"}}" {{end}}
 	}
 	return "unknown"
 }
@@ -131,7 +135,7 @@ type eventType struct {
 func newEventType(cName string) eventType {
 	var etype eventType
 	etype.Cname = cName
-	etype.Name = mixedCase(strings.TrimPrefix(cName, "PN_"))
+	etype.Name = mixedCaseTrim(cName, "PN_")
 	etype.Fname = "On" + etype.Name
 	etype.Iname = etype.Fname + "Interface"
 	return etype
@@ -141,7 +145,7 @@ var (
 	enumDefRe   = regexp.MustCompile("typedef enum {([^}]*)} pn_([a-z_]+)_t;")
 	enumValRe   = regexp.MustCompile("PN_[A-Z_]+")
 	skipEventRe = regexp.MustCompile("EVENT_NONE|REACTOR|SELECTABLE|TIMER")
-	skipFnRe    = regexp.MustCompile("attach|context|class|collect|^recv$|^send$")
+	skipFnRe    = regexp.MustCompile("attach|context|class|collect|^recv$|^send$|transport")
 )
 
 // Generate event wrappers.
@@ -183,6 +187,7 @@ type genType struct {
 	Ctype, Gotype string
 	ToGo          func(value string) string
 	ToC           func(value string) string
+	Assign        func(value string) string
 }
 
 func (g genType) printBody(out io.Writer, value string) {
@@ -237,19 +242,24 @@ func mapType(ctype string) (g genType) {
 	case "C.uint64_t":
 		g.Gotype = "uint64"
 	case "C.uint32_t":
+		g.Gotype = "uint16"
+	case "C.uint16_t":
 		g.Gotype = "uint32"
 	case "C.const char *":
-		g.Gotype = "string"
-		g.Ctype = "C.CString"
+		fallthrough
 	case "C.char *":
 		g.Gotype = "string"
 		g.Ctype = "C.CString"
+		g.ToC = func(v string) string { return fmt.Sprintf("%sC", v) }
+		g.Assign = func(v string) string {
+			return fmt.Sprintf("%sC := C.CString(%s)\n defer C.free(unsafe.Pointer(%sC))\n", v, v, v)
+		}
 	case "C.pn_seconds_t":
 		g.Gotype = "time.Duration"
 		g.ToGo = func(v string) string { return fmt.Sprintf("(time.Duration(%s) * time.Second)", v) }
 	case "C.pn_error_t *":
 		g.Gotype = "error"
-		g.ToGo = func(v string) string { return fmt.Sprintf("pnError(%s)", v) }
+		g.ToGo = func(v string) string { return fmt.Sprintf("internal.PnError(unsafe.Pointer(%s))", v) }
 	default:
 		pnId := regexp.MustCompile(" *pn_([a-z_]+)_t *\\*? *")
 		match := pnId.FindStringSubmatch(g.Ctype)
@@ -285,6 +295,7 @@ func splitArgs(argstr string) []genArg {
 	}
 	args := make([]genArg, 0)
 	for _, item := range strings.Split(argstr, ",") {
+		item = strings.Trim(item, " \n")
 		typeName := typeNameRe.FindStringSubmatch(item)
 		if typeName == nil {
 			panic(fmt.Errorf("Can't split argument type/name %#v", item))
@@ -318,6 +329,16 @@ func cArgs(args []genArg) string {
 	return l
 }
 
+func cAssigns(args []genArg) string {
+	l := "\n"
+	for _, arg := range args {
+		if arg.Assign != nil {
+			l += fmt.Sprintf("%s\n", arg.Assign(arg.Name))
+		}
+	}
+	return l
+}
+
 // Return the go name of the function or "" to skip the function.
 func goFnName(api, fname string) string {
 	// Skip class, context and attachment functions.
@@ -328,13 +349,13 @@ func goFnName(api, fname string) string {
 	case "link.get_drain":
 		return "IsDrain"
 	default:
-		return mixedCase(strings.TrimPrefix(fname, "get_"))
+		return mixedCaseTrim(fname, "get_")
 	}
 }
 
 func apiWrapFns(api, header string, out io.Writer) {
 	fmt.Fprintf(out, "type %s struct{pn *C.pn_%s_t}\n", mixedCase(api), api)
-	fmt.Fprintf(out, "func (%c %s) isNil() bool { return %c.pn == nil }\n", api[0], mixedCase(api), api[0])
+	fmt.Fprintf(out, "func (%c %s) IsNil() bool { return %c.pn == nil }\n", api[0], mixedCase(api), api[0])
 	fn := regexp.MustCompile(fmt.Sprintf(`PN_EXTERN ([a-z0-9_ ]+ *\*?) *pn_%s_([a-z_]+)\(pn_%s_t *\*[a-z_]+ *,? *([^)]*)\)`, api, api))
 	for _, m := range fn.FindAllStringSubmatch(header, -1) {
 		rtype, fname, argstr := mapType(m[1]), m[2], m[3]
@@ -345,6 +366,7 @@ func apiWrapFns(api, header string, out io.Writer) {
 		args := splitArgs(argstr)
 		fmt.Fprintf(out, "func (%c %s) %s", api[0], mixedCase(api), gname)
 		fmt.Fprintf(out, "(%s) %s { ", goArgs(args), rtype.Gotype)
+		fmt.Fprint(out, cAssigns(args))
 		rtype.printBody(out, fmt.Sprintf("C.pn_%s_%s(%c.pn%s)", api, fname, api[0], cArgs(args)))
 		fmt.Fprintf(out, "}\n")
 	}
@@ -363,10 +385,13 @@ package event
 
 import (
 	"time"
+  "unsafe"
+  "qpid.apache.org/proton/internal"
 )
 
 // #include <proton/types.h>
 // #include <proton/event.h>
+// #include <stdlib.h>
 `)
 	for _, api := range apis {
 		fmt.Fprintf(out, "// #include <proton/%s.h>\n", api)

proton-c/bindings/go/src/qpid.apache.org/proton/dummy.go
@@ -1,82 +1,0 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
-package proton
-
-import (
-	"fmt"
-	"net"
-)
-
-// Placeholder definitions to allow examples to compile.
-
-type Connection struct {
-	Server bool // Server connection does protocol negotiation
-	// FIXME aconway 2015-04-17: Other parameters to set up SSL, SASL etc.
-}
-
-// Map an AMQP connection using conn
-func (c Connection) Connect(conn net.Conn) error { return nil }
-func (c Connection) Close() error                { return nil }
-
-func (c Connection) Receiver(addr string) (*Receiver, error) {
-	// FIXME aconway 2015-04-10: dummy implementation to test examples, returns endless messages.
-	r := &Receiver{make(chan Message), make(chan struct{})}
-	go func() {
-		for i := 0; ; i++ {
-			m := NewMessage()
-			m.SetBody(fmt.Sprintf("%v-%v", addr, i))
-			select {
-			case r.Receive <- m:
-			case <-r.closed:
-				return
-			}
-		}
-	}()
-	return r, nil
-}
-
-func (c Connection) Sender(addr string) (*Sender, error) {
-	return &Sender{}, nil
-}
-
-type Receiver struct {
-	Receive chan Message
-	closed  chan struct{}
-}
-
-func (r Receiver) Close() error { return nil }
-
-type Sender struct{}
-
-func (s Sender) Send(m Message) error { fmt.Println(m.Body()); return nil }
-func (s Sender) Close() error         { return nil }
-
-// Connect makes a default client connection using conn.
-//
-// For more control do:
-//     c := Connection{}
-//     // set parameters on c
-//     c.Connect(conn)
-//
-func Connect(conn net.Conn) (Connection, error) {
-	c := Connection{}
-	c.Connect(conn)
-	return c, nil
-}

commit dcd4e47df7cb3d716d0003d01a6271ab6c663189

PROTON-827: revert mistaken change to handlers.py
Looks like there was a typo introduced by 0c11d11 that broke the builds

proton-c/bindings/python/proton/handlers.py
@@ -543,7 +543,7 @@ class CFlowController(WrappedHandler):
     def __init__(self, window=1024):
         WrappedHandler.__init__(self, lambda: pn_flowcontroller(window))
 
-oclass CHandshaker(WrappedHandler):
+class CHandshaker(WrappedHandler):
 
     def __init__(self):
         WrappedHandler.__init__(self, pn_handshaker)