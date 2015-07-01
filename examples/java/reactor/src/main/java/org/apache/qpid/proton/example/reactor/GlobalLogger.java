a29254752a08285192aae878db9986f4af569d2c
Adrian Preston <prestona@uk.ibm.com>
2015-07-01 01:22:55 +0100

    PROTON-881: Tidy up proton-j reactor examples
    Tidy up comments and more closely mirror the Python examples.

--- examples/java/reactor/src/main/java/org/apache/qpid/proton/example/reactor/GlobalLogger.java
+++ examples/java/reactor/src/main/java/org/apache/qpid/proton/example/reactor/GlobalLogger.java
@@ -28,45 +28,6 @@ import org.apache.qpid.proton.engine.BaseHandler;
 import org.apache.qpid.proton.engine.Event;
 import org.apache.qpid.proton.reactor.Reactor;
 
-/*
-# Not every event goes to the reactor's event handler. If we have a
-# separate handler for something like a scheduled task, then those
-# events aren't logged by the logger associated with the reactor's
-# handler. Sometimes this is useful if you don't want to see them, but
-# sometimes you want the global picture.
-
-class Logger:
-
-    def on_unhandled(self, name, event):
-        print "LOG:", name, event
-
-class Task:
-
-    def on_timer_task(self, event):
-        print "Mission accomplished!"
-
-class Program:
-
-    def on_reactor_init(self, event):
-        print "Hello, World!"
-        event.reactor.schedule(0, Task())
-
-    def on_reactor_final(self, event):
-        print "Goodbye, World!"
-
-r = Reactor(Program())
-
-# In addition to having a regular handler, the reactor also has a
-# global handler that sees every event. By adding the Logger to the
-# global handler instead of the regular handler, we can log every
-# single event that occurs in the system regardless of whether or not
-# there are specific handlers associated with the objects that are the
-# target of those events.
-r.global_handler.add(Logger())
-r.run()
-
- */
-
 // Not every event goes to the reactor's event handler. If we have a
 // separate handler for something like a scheduled task, then those
 // events aren't logged by the logger associated with the reactor's
