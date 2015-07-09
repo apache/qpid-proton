ed313672338e5e813cd9d9ba3b7f2dc3a9fab1fb
Dominic Evans <dominic.evans@uk.ibm.com>
2015-07-09 14:24:28 +0100

    NO-JIRA: update jenkins & travis to run tox
    Add the new 'python-tox-test' ctest (that can be run to test the python
    bindings against multiple interpreters, e.g., py2.7, py3.3 etc.) to
    the CI jobs
    
    Closes #44

--- bin/jenkins-proton-c-build.sh
+++ bin/jenkins-proton-c-build.sh
@@ -20,6 +20,21 @@ echo =========================
 which python || exit 1
 which swig || exit 1
 
+# if python-pip is available, install the python tox test tool
+RUN_TOX=false
+PIP=$(type -p pip || true)
+if [ -n $PIP ] && [ -x "$PIP" ]; then
+    ldir=$(python -c 'import site; print("%s" % site.USER_BASE)')
+    PATH="$ldir/bin:$PATH"
+    echo "PATH=$PATH"
+    if [ $VIRTUAL_ENV ]; then
+      pip install -U tox
+    else
+      pip install --user -U tox
+    fi
+    RUN_TOX=true
+fi
+
 ls
 
 rm -rf build testresults >/dev/null 2>&1
@@ -31,6 +46,8 @@ cmake ${CMAKE_FLAGS} ..
 cmake --build . --target install
 
 echo Running tests
+
+$RUN_TOX && ctest -V -R 'python-tox-test'
 
 source config.sh
 
