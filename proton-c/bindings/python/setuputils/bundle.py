d5b1036c3c129657cd63ecbfbf489f674a17f5f4
Robert Gemmell <robbie@apache.org>
2016-10-10 16:28:47 +0100

    PROTON-1323: update versions on master for 0.16.0-SNAPSHOT, inc python bundle version

--- proton-c/bindings/python/setuputils/bundle.py
+++ proton-c/bindings/python/setuputils/bundle.py
@@ -32,10 +32,10 @@ from . import log
 #-----------------------------------------------------------------------------
 # Constants
 #-----------------------------------------------------------------------------
-min_qpid_proton = (0, 15, 0)
+min_qpid_proton = (0, 16, 0)
 min_qpid_proton_str = "%i.%i.%i" % min_qpid_proton
 
-bundled_version = (0, 15, 0)
+bundled_version = (0, 16, 0)
 bundled_version_str = "%i.%i.%i" % bundled_version
 libqpid_proton = "qpid-proton-%s.tar.gz" % bundled_version_str
 libqpid_proton_url = ("http://www.apache.org/dist/qpid/proton/%s/%s" %
