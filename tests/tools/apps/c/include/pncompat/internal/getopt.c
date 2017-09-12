d7f6e0662980fb8196dfd3b536210e76f851c6cd
Alan Conway <aconway@redhat.com>
2017-09-12 13:56:35 -0400

    PROTON-1585: Remove un-necessary include/pncompat from C examples.

--- tests/tools/apps/c/include/pncompat/internal/getopt.c
+++ tests/tools/apps/c/include/pncompat/internal/getopt.c
@@ -4,7 +4,7 @@
 *
 * Copyright (c)2002-2003 Mark K. Kim
 * All rights reserved.
-* 
+*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
 * are met:
