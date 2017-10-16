a518b1ce98ca90ed9ce7541dde789004697338a3
Alan Conway <aconway@redhat.com>
2017-10-16 13:33:48 +0100

    NO-JIRA: Fix tests/fdlimit.py to work on python 2.6

--- proton-c/src/tests/fdlimit.py
+++ proton-c/src/tests/fdlimit.py
@@ -41,48 +41,43 @@ class LimitedBroker(object):
 # Check if we can run prlimit to control resources
 try:
     Proc(["prlimit"]).wait_exit()
-    has_prlimit = True
+
+    class FdLimitTest(ProcTestCase):
+
+        def proc(self, *args, **kwargs):
+            """Skip valgrind for all processes started by this test"""
+            return super(FdLimitTest, self).proc(*args, valgrind=False, **kwargs)
+
+        def test_fd_limit_broker(self):
+            """Check behaviour when running out of file descriptors on accept"""
+            # Not too many FDs but not too few either, some are used for system purposes.
+            fdlimit = 256
+            with LimitedBroker(self, fdlimit) as b:
+                receivers = []
+                # Start enough receivers to use all FDs, make sure the broker logs an error
+                for i in xrange(fdlimit+1):
+                    receivers.append(self.proc(["receive", "", b.port, str(i)]))
+
+                # Note: libuv silently swallows EMFILE/ENFILE errors so there is no error reporting.
+                # The epoll proactor will close the users connection with the EMFILE/ENFILE error
+                if "TRANSPORT_CLOSED" in b.proc.out:
+                    self.assertIn("open files", b.proc.out)
+
+                # All FDs are now in use, send attempt should fail or hang
+                self.assertIn(self.proc(["send", "", b.port, "x"]).poll(), [1, None])
+
+                # Kill receivers to free up FDs
+                for r in receivers:
+                    r.kill()
+                for r in receivers:
+                    r.wait_exit(expect=None)
+                # send/receive should succeed now
+                self.assertIn("10 messages sent", self.proc(["send", "", b.port]).wait_exit())
+                self.assertIn("10 messages received", self.proc(["receive", "", b.port]).wait_exit())
+
+    if __name__ == "__main__":
+        main()
+
 except:
-    has_prlimit = False
+    print "Skipping test: prlimit not available"
 
-class FdLimitTest(ProcTestCase):
-
-    def setUp(self):
-        global has_prlimit
-        if not has_prlimit:
-            self.skipTest("prlimit not available")
-        super(FdLimitTest, self).setUp()
-
-    def proc(self, *args, **kwargs):
-        """Skip valgrind for all processes started by this test"""
-        return super(FdLimitTest, self).proc(*args, valgrind=False, **kwargs)
-
-    def test_fd_limit_broker(self):
-        """Check behaviour when running out of file descriptors on accept"""
-        # Not too many FDs but not too few either, some are used for system purposes.
-        fdlimit = 256
-        with LimitedBroker(self, fdlimit) as b:
-            receivers = []
-            # Start enough receivers to use all FDs, make sure the broker logs an error
-            for i in xrange(fdlimit+1):
-                receivers.append(self.proc(["receive", "", b.port, str(i)]))
-
-            # Note: libuv silently swallows EMFILE/ENFILE errors so there is no error reporting.
-            # The epoll proactor will close the users connection with the EMFILE/ENFILE error
-            if "TRANSPORT_CLOSED" in b.proc.out:
-                self.assertIn("open files", b.proc.out)
-
-            # All FDs are now in use, send attempt should fail or hang
-            self.assertIn(self.proc(["send", "", b.port, "x"]).poll(), [1, None])
-
-            # Kill receivers to free up FDs
-            for r in receivers:
-                r.kill()
-            for r in receivers:
-                r.wait_exit(expect=None)
-            # send/receive should succeed now
-            self.assertIn("10 messages sent", self.proc(["send", "", b.port]).wait_exit())
-            self.assertIn("10 messages received", self.proc(["receive", "", b.port]).wait_exit())
-
-if __name__ == "__main__":
-    main()
