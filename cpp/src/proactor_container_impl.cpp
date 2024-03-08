674b0c6dbddc3a20bab17c60381a145d7b692ff4
Andrew Stitcher <astitcher@apache.org>
2024-03-07 21:19:03 -0500

    PROTON-2792: [C++] check that scheduled tasks are active under lock
    Previously we checked whether the tasks were active without locking
    which was bad.

--- cpp/src/proactor_container_impl.cpp
+++ cpp/src/proactor_container_impl.cpp
@@ -542,9 +542,16 @@ void container::impl::run_timer_jobs() {
     // NB. We copied the due tasks in reverse order so execute from end
 
     for (int i = tasks.size()-1; i>=0; --i) {
-        if(is_active_.count(tasks[i].w_handle)) {
-            tasks[i].task();
-            is_active_.erase(tasks[i].w_handle);
+        const auto& task = tasks[i];
+        bool active;
+
+        {
+          GUARD(deferred_lock_);
+          // NB. erase returns the number of items erased
+          active = is_active_.erase(task.w_handle);
+        }
+        if (active) {
+            task.task();
         }
     }
 }
