11b67fc4edab06ed0f55526b75cab5ea2650a761
Andrew Stitcher <astitcher@apache.org>
2024-12-06 15:53:43 -0500

    PROTON-2865: [Python] Make the docs build correctly
    * This tells Sphinx to mock the import of the cproton_ffi module so that
      it doesn't need to be present for building the docs.
    
    * Also change the doc config to get trid of a couple of needless
      warnings.

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+#language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -100,7 +100,7 @@ html_theme = 'sphinxdoc'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -192,3 +192,4 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+autodoc_mock_imports = ['cproton_ffi']
